f4947fe0c0987f8144685e37aba8bb87cd6325db
SolopiyA <artem.solopiy.gdc@ts.fujitsu.com>
2015-07-07 14:29:59 +0300

    Auth

--- src/web/basic/controllers/ChartDataController.php
+++ src/web/basic/controllers/ChartDataController.php
@@ -12,17 +12,37 @@ namespace app\controllers;
 use app\businessLogic\contracts\weatherData\filters\WeatherDataRetrieveLastFilter;
 use app\businessLogic\implementation\weatherData\WeatherDataManager;
 use app\models\ChartDataItemModel;
+use yii\filters\auth\HttpBasicAuth;
 
 class ChartDataController extends \yii\rest\Controller {
-    public function actionGet() {
-        $manager = new WeatherDataManager();
-        $items = $manager->retrieveChartData(new WeatherDataRetrieveLastFilter());
-        foreach($items as $item) {
+
+    /**
+     * @param array $data
+     * @return \Generator
+     */
+    private static function &chartDataToModel(array &$data) {
+        foreach ($data as $item) {
             $res = new ChartDataItemModel();
             $res->id = $item->key;
             $res->temperature = $item->value;
             $res->dateTime = $item->dateTime->format(\DateTime::ISO8601);
             yield $res;
         }
+    }
+
+    public function behaviors() {
+        $behaviors                  = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => HttpBasicAuth::className(),
+        ];
+
+        return $behaviors;
+    }
+
+    public function actionIndex() {
+        $manager = new WeatherDataManager();
+        $items   = $manager->retrieveChartData(new WeatherDataRetrieveLastFilter());
+
+        return iterator_to_array(self::chartDataToModel($items));
     }
 }
